961169984e6cd2c94952980565172b9a70336a55
Hiromi ISHII <konn.jinro@gmail.com>
2023-08-21 20:51:52 +0900

    Stop using unsafeCoerce

--- src/Data/Array/Mutable/Unlifted/Linear/Extra.hs
+++ src/Data/Array/Mutable/Unlifted/Linear/Extra.hs
@@ -14,14 +14,14 @@ module Data.Array.Mutable.Unlifted.Linear.Extra (
 
 import Data.Alloc.Linearly.Token
 import Data.Array.Mutable.Unlifted.Linear
-import GHC.Exts (unsafeCoerce#)
-import qualified GHC.Exts as GHC
 import Prelude.Linear
-import qualified Prelude as P
+import qualified Unsafe.Linear as Unsafe
+
+data ArrayC a = ArrayC (Array# a)
 
 allocL :: Linearly %1 -> Int -> a -> Array# a
-allocL l (GHC.I# s) a =
+{-# ANN allocL "HLint: ignore Avoid lambda" #-}
+allocL l s x =
   consume l & \() ->
-    case GHC.runRW# P.$ GHC.newArray# s a of
-      (# _, arr #) -> unsafeCoerce# arr
+    alloc s x (Unsafe.toLinear (\arr# -> Ur (ArrayC arr#))) & \(Ur (ArrayC arr)) -> arr
 {-# NOINLINE allocL #-} -- prevents runRW# from floating outwards
