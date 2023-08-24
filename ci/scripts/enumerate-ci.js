552191784f32d629ea3f6c6a7bae693229108c39
Hiromi ISHII <konn.jinro@gmail.com>
2023-08-24 16:34:42 +0900

    Absolute makes sense

--- ci/scripts/enumerate-ci.js
+++ ci/scripts/enumerate-ci.js
@@ -2,11 +2,10 @@ module.exports = async ({ github, context, core, glob, io, require }) => {
   const fs = require("fs");
   const globber = await glob.create("ci/configs/*.project");
   const files = await globber.glob();
-  const plans = files.map((fullpath) => {
-    const path = fullpath.replace(/^.+\//, "ci/configs/");
-    const name = fullpath.replaceAll(/.+\/|\.project$/g, "");
+  const plans = files.map((path) => {
+    const name = path.replaceAll(/.+\/|\.project$/g, "");
     const match = fs
-      .readFileSync(fullpath, "utf-8")
+      .readFileSync(path, "utf-8")
       .match(/with-compiler:\s*ghc-([\d\.]+)/);
     const ghc = match[1];
 
