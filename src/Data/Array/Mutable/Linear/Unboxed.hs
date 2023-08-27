ff7ed2413568dab2eecfd05680c981b39e78ae95
Hiromi ISHII <konn.jinro@gmail.com>
2023-08-27 17:18:50 +0900

    perf: Uses unboxed mutable array directly

--- src/Data/Array/Mutable/Linear/Unboxed.hs
+++ src/Data/Array/Mutable/Linear/Unboxed.hs
@@ -39,54 +39,60 @@ module Data.Array.Mutable.Linear.Unboxed (
   findIndex,
 ) where
 
-import qualified Control.Monad as P
-import Control.Monad.ST.Strict (runST)
 import Data.Alloc.Linearly.Token
 import Data.Alloc.Linearly.Token.Unsafe (HasLinearWitness)
 import qualified Data.Array.Mutable.Linear.Class as C
-import Data.Coerce (coerce)
 import Data.Vector.Unboxed (Unbox)
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Unboxed.Mutable as MU
+import GHC.Exts (RealWorld, runRW#)
+import GHC.IO (unIO)
 import GHC.Stack (HasCallStack)
 import Prelude.Linear hiding (map)
 import qualified Unsafe.Linear as Unsafe
 import qualified Prelude as P
 
-newtype UArray a = UArray (U.Vector a)
+newtype UArray a = UArray (MU.MVector RealWorld a)
   deriving anyclass (HasLinearWitness)
 
 alloc :: (HasCallStack, U.Unbox a) => Int -> a -> (UArray a %1 -> Ur b) %1 -> Ur b
 {-# NOINLINE alloc #-}
 alloc n x f
   | n < 0 = error ("UArray.alloc: Negative length: " <> show n) f
-  | otherwise = f (UArray $ U.replicate n x)
+  | otherwise = case runRW# (unIO $ MU.replicate n x) of
+      (# _, mu #) -> f (UArray mu)
 
 allocL :: (HasCallStack, U.Unbox a) => Linearly %1 -> Int -> a -> UArray a
 {-# NOINLINE allocL #-}
 allocL l n x
   | n < 0 = error ("UArray.alloc: Negative length: " <> show n) l
   | otherwise =
-      consume l & \() -> UArray (U.replicate n x)
+      consume l & \() -> case runRW# (unIO $ MU.replicate n x) of
+        (# _, mu #) -> UArray mu
 
 unsafeAlloc :: U.Unbox a => Int -> (UArray a %1 -> Ur b) %1 -> Ur b
 {-# NOINLINE unsafeAlloc #-}
 unsafeAlloc n (f :: UArray a %1 -> b) =
-  f $ UArray $ runST (U.unsafeFreeze P.=<< MU.unsafeNew n)
+  case runRW# (unIO $ MU.unsafeNew n) of
+    (# _, mu #) -> f $ UArray mu
 
 unsafeAllocL :: U.Unbox a => Linearly %1 -> Int -> UArray a
 {-# NOINLINE unsafeAllocL #-}
-unsafeAllocL l n = l `lseq` UArray (runST (U.unsafeFreeze P.=<< MU.unsafeNew n))
+unsafeAllocL l n =
+  l `lseq` case runRW# (unIO $ MU.unsafeNew n) of
+    (# _, mu #) -> UArray mu
 
 unsafeAllocBeside :: U.Unbox a => Int -> UArray b %1 -> (UArray a, UArray b)
 {-# NOINLINE unsafeAllocBeside #-}
 unsafeAllocBeside n (UArray orig) =
-  (UArray $ runST (U.unsafeFreeze P.=<< MU.unsafeNew n), UArray orig)
+  case runRW# (unIO $ MU.unsafeNew n) of
+    (# _, mu #) -> (UArray mu, UArray orig)
 
 fill :: U.Unbox a => a -> UArray a %1 -> UArray a
-fill a = Unsafe.toLinear \(UArray arr) -> runST do
-  flip MU.set a P.=<< U.unsafeThaw arr
-  P.pure P.$ UArray arr
+{-# NOINLINE fill #-}
+fill a = Unsafe.toLinear \(UArray arr) ->
+  case runRW# $ unIO $ MU.set arr a of
+    (# _, () #) -> UArray arr
 
 -- | Check if given index is within the Array, otherwise panic.
 assertIndexInRange :: (HasCallStack, Unbox a) => Int -> UArray a %1 -> UArray a
@@ -98,7 +104,7 @@ assertIndexInRange i arr =
 
 size :: U.Unbox a => UArray a %1 -> (Ur Int, UArray a)
 size = Unsafe.toLinear \(UArray mu) ->
-  (Ur (U.length mu), UArray mu)
+  (Ur (MU.length mu), UArray mu)
 
 set :: (HasCallStack, U.Unbox a) => Int -> a -> UArray a %1 -> UArray a
 set i x arr =
@@ -106,38 +112,42 @@ set i x arr =
 
 unsafeSet :: U.Unbox a => Int -> a -> UArray a %1 -> UArray a
 {-# NOINLINE unsafeSet #-}
-unsafeSet i a = Unsafe.toLinear \arr0@(UArray arr) -> runST do
-  mu <- U.unsafeThaw arr
-  MU.write mu i a
-  P.pure arr0
+unsafeSet i a = Unsafe.toLinear \arr0@(UArray mu) ->
+  case runRW# (unIO $ MU.unsafeWrite mu i a) of
+    (# _, () #) -> arr0
 
 get :: (HasCallStack, U.Unbox a) => Int -> UArray a %1 -> (Ur a, UArray a)
 get i arr = unsafeGet i (assertIndexInRange i arr)
 
 unsafeGet :: U.Unbox a => Int -> UArray a %1 -> (Ur a, UArray a)
 {-# NOINLINE unsafeGet #-}
-unsafeGet i = Unsafe.toLinear \(UArray mu) ->
-  (Ur (U.unsafeIndex mu i), UArray mu)
+unsafeGet i = Unsafe.toLinear \arr0@(UArray mu) ->
+  case runRW# (unIO $ MU.unsafeRead mu i) of
+    (# _, x #) -> (Ur x, arr0)
 
 -- | /O(1)/ freeze
-freeze :: UArray a %1 -> Ur (U.Vector a)
-freeze = Unsafe.toLinear (Ur P.. coerce)
+freeze :: U.Unbox a => UArray a %1 -> Ur (U.Vector a)
+{-# NOINLINE freeze #-}
+freeze = Unsafe.toLinear \(UArray mu) ->
+  case runRW# $ unIO $ U.unsafeFreeze mu of
+    (# _, uv #) -> Ur uv
 
 unsafeSlice :: (U.Unbox a) => Int -> Int -> UArray a %1 -> (UArray a, UArray a)
 {-# NOINLINE unsafeSlice #-}
 unsafeSlice start len = Unsafe.toLinear \(UArray mu) ->
-  (UArray mu, UArray $! U.force $! U.unsafeSlice start len mu)
+  case runRW# $ unIO $ MU.clone $! MU.unsafeSlice start len mu of
+    (# _, mu' #) -> (UArray mu, UArray mu')
 
 unsafeResize :: (U.Unbox a) => Int -> UArray a %1 -> UArray a
 {-# NOINLINE unsafeResize #-}
 unsafeResize n = Unsafe.toLinear \(UArray arr) ->
-  case compare n (U.length arr) of
-    LT -> UArray $! U.force P.$! U.unsafeTake n arr
+  case compare n (MU.length arr) of
+    LT ->
+      case runRW# $ unIO $ MU.clone $ MU.unsafeTake n arr of
+        (# _, arr #) -> UArray arr
     EQ -> UArray arr
-    GT -> runST do
-      marr <- U.unsafeThaw arr
-      marr <- MU.unsafeGrow marr (n - MU.length marr)
-      UArray P.<$!> U.unsafeFreeze marr
+    GT -> case runRW# $ unIO $ MU.unsafeGrow arr (n - MU.length arr) of
+      (# _, arr #) -> UArray arr
 
 {- | Note: 'Unbox'ed vectors should be regarded as an n-tuple of ByteArray;
 So we can just drop it.
@@ -148,7 +158,8 @@ instance Consumable (UArray a) where
 
 instance (U.Unbox a) => Dupable (UArray a) where
   dup2 = Unsafe.toLinear \(UArray mu) ->
-    (UArray mu, UArray $! U.force mu)
+    case runRW# $ unIO $ MU.clone mu of
+      (# _, mu' #) -> (UArray mu, UArray mu')
   {-# NOINLINE dup2 #-}
 
 fromList :: U.Unbox a => [a] -> (UArray a %1 -> Ur b) %1 -> Ur b
