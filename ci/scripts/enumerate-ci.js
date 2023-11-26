e907f564ce1bc8d8f081923c176e1f8145c64726
Hiromi ISHII <konn.jinro@gmail.com>
2023-11-26 15:48:51 +0900

    ci: Includes GHC 9.8.1 as a HEAD

--- ci/scripts/enumerate-ci.js
+++ ci/scripts/enumerate-ci.js
@@ -18,16 +18,7 @@ module.exports = async ({ github, context, core, glob, io, require }) => {
     return { path, name, ghc, is_head };
   }
 
-  const plans = files.map((fullpath) => {
-    const path = fullpath.replace(/^.+\//, "ci/configs/");
-    const name = fullpath.replaceAll(/.+\/|\.project$/g, "");
-    const match = fs
-      .readFileSync(fullpath, "utf-8")
-      .match(/with-compiler:\s*ghc-([\d\.]+)/);
-    const ghc = match[1];
-
-    return { path, name, ghc };
-  });
+  const plans = files.map(generate_obj) + files_falliable.map(generate_obj);
   core.info(`plan: ${JSON.stringify(plans)}`);
   core.setOutput("plan", JSON.stringify(plans));
 };
