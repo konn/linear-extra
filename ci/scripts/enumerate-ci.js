a0440c667cf25ba0e041b28da9a041c28c4ec155
Hiromi ISHII <konn.jinro@gmail.com>
2023-11-26 15:47:34 +0900

    ci: Allow HEAD to fail

--- ci/scripts/enumerate-ci.js
+++ ci/scripts/enumerate-ci.js
@@ -1,7 +1,23 @@
 module.exports = async ({ github, context, core, glob, io, require }) => {
   const fs = require("fs");
   const globber = await glob.create("ci/configs/*.project");
+  const globber_falliable = await glob.create("ci/configs/head/*.project");
   const files = await globber.glob();
+  const files_falliable = await globber_falliable.glob();
+
+  function generate_obj(fullpath) {
+    const is_head = fullpath.test(/configs\/head/);
+    const prefix = is_head ? "ci/configs/head/" : "ci/configs/";
+    const path = fullpath.replace(/^.+\//, prefix);
+    const name = fullpath.replaceAll(/.+\/|\.project$/g, "");
+    const match = fs
+      .readFileSync(fullpath, "utf-8")
+      .match(/with-compiler:\s*ghc-([\d\.]+)/);
+    const ghc = match[1];
+
+    return { path, name, ghc, is_head };
+  }
+
   const plans = files.map((fullpath) => {
     const path = fullpath.replace(/^.+\//, "ci/configs/");
     const name = fullpath.replaceAll(/.+\/|\.project$/g, "");
